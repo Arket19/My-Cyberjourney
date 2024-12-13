690bcb31680e48817a0fbeb5392466be45a75561
Arket19 <73239391+Arket19@users.noreply.github.com>
2024-12-13 15:38:56 +0100

    Tarde

--- Automation/createHTMLfromMD.py
+++ Automation/createHTMLfromMD.py
@@ -2,55 +2,59 @@ import os
 import zipfile
 import shutil
 import markdown
+import json
+from datetime import datetime
 
 # Crear el contenido HTML
 def generate_html(md_content, title):
-
+    # Modificar para añadir clase "title" a la primera etiqueta h1
+    md_content = md_content.replace('<h1>', '<h1 class="title">', 1)  # Solo modifica el primer h1
     html_template = f"""
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset=\"UTF-8\">
-            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-            <title>{title} - My Cyberjourney</title>
-            <link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">
-            <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
-            <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-            <link href=\"{title}.css\" rel=\"stylesheet\">
-            <link href=\"https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe:ital@0;1&display=swap\" rel=\"stylesheet\">
-            <link href=\"https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap\" rel=\"stylesheet\">
-        </head>
-        <body>
-            <!-- Menú -->
-            <nav class=\"w3-bar\">
-                <div class=\"logo\">
-                    <p>My Cyberjourney</p>
-                </div>
-                <div class=\"w3-bar-item w3-right w3-hide-large w3-hover-opacity\" onclick=\"w3_open()\">
-                    <i class=\"fa fa-bars w3-xlarge\"></i>
-                </div>
-                <div class=\"w3-hide-small\">
-                    <ul class=\"elementos-menu\">
-                        <li class=\"w3-bar-item\"><a href=\"index.html\">Home</a></li>
-                        <li class=\"w3-bar-item\"><a href=\"curriculum/curriculum.html\">Resume (CV)</a></li>
-                        <li class=\"w3-bar-item\"><a href=\"writeups/writeups.html\">Writeups</a></li>
-                        <li class=\"w3-bar-item\"><a href=\"learning/learning.html\">Learning Journey</a></li>
-                    </ul>
-                </div>
-            </nav> 
-            <div class=\"content\">
-                {md_content}
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{title} - My Cyberjourney</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="{title}.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe:ital@0;1&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Menú -->
+        <nav class="w3-bar">
+            <div class="logo">
+                <p>My Cyberjourney</p>
             </div>
-        </body>
-    </html>
+            <div class="w3-bar-item w3-right w3-hide-large w3-hover-opacity" onclick="w3_open()">
+                <i class="fa fa-bars w3-xlarge"></i>
+            </div>
+            <div class="w3-hide-small">
+                <ul class="elementos-menu">
+                    <li class="w3-bar-item"><a href="index.html">Home</a></li>
+                    <li class="w3-bar-item"><a href="curriculum/curriculum.html">Resume (CV)</a></li>
+                    <li class="w3-bar-item"><a href="writeups/writeups.html">Writeups</a></li>
+                    <li class="w3-bar-item"><a href="learning/learning.html">Learning Journey</a></li>
+                </ul>
+            </div>
+        </nav> 
+        <div class="content">
+            {md_content}
+        </div>
+    </body>
+</html>
 """
     return html_template
 
+
 def process_zip_files():
-    # Guardar las rutas de las carpetas y el archivo de errores
-    root_dir = os.path.dirname(os.path.abspath(__file__))  
-    files_dir = os.path.join(root_dir, 'files') 
-    writeups_dir = os.path.join(root_dir, '..', 'writeups') 
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    files_dir = os.path.join(root_dir, 'files')
+    writeups_dir = os.path.join(root_dir, '..', 'writeups')
+    json_file_path = os.path.join(root_dir,'..','writeups.json')
     
     error_file = os.path.join(root_dir, 'error.txt')
     if os.path.exists(error_file):
@@ -77,12 +81,18 @@ def process_zip_files():
             error_log.write(errors)
         return
 
-    # Procesar cada archivo zip encontrado en la carpeta
+    # Leer o crear el archivo JSON
+    if os.path.exists(json_file_path):
+        with open(json_file_path, 'r', encoding='utf-8') as json_file:
+            writeups_data = json.load(json_file)
+    else:
+        writeups_data = []
+
+    # Procesar cada archivo zip
     for zip_file in zip_files:
         zip_file_path = os.path.join(files_dir, zip_file)
         folder_name = os.path.splitext(zip_file)[0]
 
-        # Crear carpeta temporal para extraer el contenido del ZIP
         extracted_folder = os.path.join(files_dir, folder_name)
         os.makedirs(extracted_folder, exist_ok=True)
 
@@ -91,10 +101,8 @@ def process_zip_files():
             with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
                 zip_ref.extractall(extracted_folder)
 
-            # Localizar los archivos .md en el contenido extraído
             md_files = [f for f in os.listdir(extracted_folder) if f.endswith('.md')]
 
-            # Si hay más de un archivo .md, registrar un error y continuar
             if len(md_files) != 1:
                 errors += f"There are more than one .md files in '{zip_file}'.\n"
                 continue
@@ -102,33 +110,48 @@ def process_zip_files():
             md_file = md_files[0]
             md_file_path = os.path.join(extracted_folder, md_file)
 
-            # Crear la carpeta del writeup
             writeup_folder = os.path.join(writeups_dir, folder_name.capitalize())
 
-            # Si la carpeta ya existe, registrar un error y detener el programa
             if os.path.exists(writeup_folder):
                 errors += f"The folder '{writeup_folder}' already exists.\n"
                 break
 
             os.makedirs(writeup_folder, exist_ok=True)
 
-            # Leer el archivo Markdown
             with open(md_file_path, 'r', encoding='utf-8') as file:
                 md_content = file.read()
 
-            # Convertir de Markdown a HTML
+            # Extraer las etiquetas y eliminar la línea de "Etiquetas:"
+            lines = md_content.splitlines()
+
+            tags = []
+            if len(lines) > 1 and lines[2].startswith("Etiquetas:"):
+                tags_line = lines.pop(2)  # Eliminar la línea de etiquetas
+                tags = tags_line.replace("Etiquetas: ", "").split(",")
+                tags = [tag.strip() for tag in tags]
+
+            # El contenido MD sin la línea de etiquetas
+            md_content = "\n".join(lines)
+
+            # Añadir la entrada del writeup al JSON
+            writeup_entry = {
+                "title": f"HTB: {folder_name.capitalize()}",
+                "url": f"writeups/{folder_name.capitalize()}/{folder_name.capitalize()}.html",
+                "tags": tags,
+                "date": datetime.now().strftime("%d/%m/%Y")
+            }
+            writeups_data.insert(0, writeup_entry)  # Insertar como la primera entrada
+
+            # Generar el archivo HTML
             html_content = markdown.markdown(md_content)
             title = folder_name.capitalize()
             html_output = os.path.join(writeup_folder, f'{title}.html')
             css_output = os.path.join(writeup_folder, f'{title}.css')
 
-            # Crear el archivo .html
             with open(html_output, 'w', encoding='utf-8') as html_file:
                 html_file.write(generate_html(html_content, title))
 
-            # Crear el archivo .css
-            css_content =  """
-        /* General */
+            css_content = """/* General */
 body {
     font-family: 'Roboto Serif', serif;
     background-color: #222222; /* Fondo negro */
@@ -189,6 +212,9 @@ nav ul li a {
 
 .title{
     color: white;
+    font-family: 'Alumni Sans Pinstripe', sans-serif;
+    font-size: 2.5rem;
+    border-bottom: 2px solid #00bcd4;
 }
 h1 {
     padding-top: 4rem;
@@ -243,22 +269,24 @@ img {
 p {
     margin-bottom: 15px;
 }
-        
-        """
+        """  
+           
             with open(css_output, 'w', encoding='utf-8') as css_file:
                 css_file.write(css_content)
 
-            # Mover las imágenes a la carpeta del writeup
+            # Mover las imágenes
             for item in os.listdir(extracted_folder):
                 if item.lower().endswith(('.png', '.jpg', '.jpeg')):
                     img_path = os.path.join(extracted_folder, item)
                     shutil.move(img_path, os.path.join(writeup_folder, item))
 
         finally:
-            # Eliminar la carpeta temporal en todos los casos
             shutil.rmtree(extracted_folder)
 
-    # Si hubo errores, escribirlos en error.txt
+    # Guardar el archivo JSON actualizado
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(writeups_data, json_file, indent=4)
+
     if errors:
         with open(error_file, 'w', encoding='utf-8') as error_log:
             error_log.write(errors)
